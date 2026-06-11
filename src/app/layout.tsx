a9d8115ea6a4c677af9f11d0e48b2befdfe9fd7b
AbdoHesham01 <abdohesham203@gmail.com>
2026-06-11 07:21:06 +0300

    Defer font CSS (non-render-blocking), preload poster image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,14 @@ export default function RootLayout({
         <link rel="preload" as="font" href="https://fonts.cdnfonts.com/s/85546/Satoshi-Black.woff" crossOrigin="anonymous" />
         <link rel="preload" as="font" href="https://fonts.cdnfonts.com/s/85546/Satoshi-Bold.woff" crossOrigin="anonymous" />
         <link rel="preload" as="font" href="https://fonts.cdnfonts.com/s/85546/Satoshi-Regular.woff" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/satoshi" />
+        <link rel="preload" as="image" href="/dialexpert/hero-poster.jpg" />
+        <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/satoshi" media="print" {...{ "onLoad": "this.media='all'" } as any} />
         <style dangerouslySetInnerHTML={{ __html: "@font-face{font-family:Satoshi;font-style:normal;font-weight:900;font-display:swap;src:url(https://fonts.cdnfonts.com/s/85546/Satoshi-Black.woff) format('woff')}@font-face{font-family:Satoshi;font-style:normal;font-weight:700;font-display:swap;src:url(https://fonts.cdnfonts.com/s/85546/Satoshi-Bold.woff) format('woff')}@font-face{font-family:Satoshi;font-style:normal;font-weight:400;font-display:swap;src:url(https://fonts.cdnfonts.com/s/85546/Satoshi-Regular.woff) format('woff')}" }} />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" media="print" {...{ "onLoad": "this.media='all'" } as any} />
+        <noscript>
+          <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/satoshi" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" />
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
